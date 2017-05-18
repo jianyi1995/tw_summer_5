195671c5f6c16153075cdb4747eabd5286b25893
shaowenhan <1138500211@qq.com>
2017-05-18 23:25:22 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,18 +1,45 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+    console.log(`o) 最小值 = ${sequence.minimum()}`);
+    console.log(`o) 最小值 = ${sequence.maximum()}`);
+    console.log(`o) 元素数量 = ${sequence.length}`);
+    console.log(`o) 平均值 = ${sequence.average()}`);
+
+
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    this.array = input
+    this.length = input.length
   }
 
   minimum() {
+  	let min = this.array[0]
     // Write your code here
+    for(let v of this.array){
+    	if(v < min)
+    		min = v
+    }
+    return min
+  }
+
+  maximum(){
+  	let max = this.array[0]
+  	for(let v of this.array){
+  		if(v > max)
+  			max = v
+  	}
+  	return max
   }
 
   // Write your code here
+  average(){
+  	let res = 0
+  	for(let v of this.array)
+  		res += v
+  	res /= this.length
+  	return res.toFixed(2)
+  }
 }
